f30c11b4355b25aa245adc9318822b428ab1e849
山田翔大 <y.sholine06@icloud.com>
2025-08-06 14:01:58 +0900

    Update notifier.py

--- notifier.py
+++ notifier.py
@@ -3,7 +3,7 @@ import requests
 from datetime import datetime
 
 # 自分のDiscordのWebhook URLを設定
-DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/1377939338293280849/URJZ_dSkB2h8HtdQWHlGHmiOkvHgCojsbQGmDrW0lFFpSyVY29pcLYF_TxEVyd6-Pso3'
+DISCORD_WEBHOOK_URL = 'https://discord.com/api/webhooks/YOUR/DISCORD/URL'
 
 def send_discord_notify(message: str):
     now_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -16,4 +16,4 @@ def send_discord_notify(message: str):
     if response.status_code != 204:
         print(f"[エラー] 通知失敗：{response.status_code} - {response.text}")
     else:
-        print("[成功] 通知を送信したぜ")
+        print("[成功] 通知を送信しました")
